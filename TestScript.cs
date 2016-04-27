1e15adbadc6b3bd8ee9b73a69edc6d9b30b113d6
Luke Amer <lukeshopz@yahoo.co.uk>
2016-04-27 19:25:34 +0100

    Updated

--- TestScript.cs
+++ TestScript.cs
@@ -5,14 +5,15 @@ public class TestScript : MonoBehaviour
 {
     // Test Scripts
 
-    int num = 10;
+    float num = 100.0f;
 
 
     void Start()
     {
 
-        num = num + 20;
-        Debug.Log(num);
+        num = num + 20.0f;
+        num = 0.0f;
+        Debug.Log("Result: " + num);
 
     }
 
