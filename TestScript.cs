a583744ca82155582ea8295dbf882ae03d003436
Luke Amer <lukeamer@me.com>
2016-04-27 20:05:12 +0100

    Testing out Ref

--- TestScript.cs
+++ TestScript.cs
@@ -5,27 +5,21 @@ public class TestScript : MonoBehaviour
 {
     // Test Scripts
 
-    int num2 = 10;
-    int num3 = 1;
-
-    float num = 100.0f;
-
 
     void Start()
     {
-        num2 *= 50;
-        num2 = num2 + 20;
-        
-        Debug.Log(num2);
+        int i = 10;
 
-        Debug.Log("Alright m8?");
+        AddNum(ref i);
 
-        num = num + 20.0f;
-        num = 0.0f;
-        Debug.Log("Result: " + num);
+        Debug.Log(i);
 
     }
 
+    void AddNum(ref int num)
+    {
+        num += 10;
+    }
 
 
 }
