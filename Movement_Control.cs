28d8e9a5105d0c6562b74dfd525d375e16e58683
Rob <robert.p.webb@live.com>
2016-04-28 20:20:51 +0100

    Controller

---
+++ Movement_Control.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_Control : MonoBehaviour {
+
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveX = Input.GetAxis("JoyX") * moveSpeed * Time.deltaTime;
+        float Vertical = Input.GetAxis("JoyY") * moveSpeed * Time.deltaTime;
+
+        float moveY4 = Input.GetAxis("4th") * moveSpeed * Time.deltaTime;
+        //float rotY = Input.GetAxis("RightJoyY") * move * Time.deltaTime;
+
+
+
+        if (Input.GetAxis("JoyY") != 0)
+        {
+            //transform.position += new Vector3(0, 0, Vertical);
+            //transform.localPosition += new Vector3(0, 0, Vertical);
+            //transform.position = new Vector3(0,moveY4,0) * Vertical;
+        }
+       // if (Input.GetAxis("JoyY") != 0)
+       // {
+       //     transform.Rotate(0, moveY4, 0);
+       // }
+
+        if (Input.GetAxis("4th") != 0)
+        {
+            transform.Rotate(0, moveY4, 0);
+          //  transform.rotation = Quaternion.Slerp(Vertical, -Vertical, Time.deltaTime * 6);
+        }
+
+       
+
+
+
+
+
+
+
+	}
+}
---
+++ Turret_Rotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret_Rotation : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotY = Input.GetAxis("9") * speed * Time.deltaTime;
+        float rotY2 = Input.GetAxis("10") * speed * Time.deltaTime;
+
+        if (Input.GetAxis("9") != 0)
+        {
+
+       // rotY = Mathf.Clamp(rotY, -60f, 60f);
+        //transform.Rotate(0,rotY * speed *  Time.deltaTime,0);
+        // transform.localEulerAngles = new Vector3(0, rotY *speed * Time.deltaTime, 0);
+        transform.Rotate(0, -rotY, 0);
+        //transform.rotation = rot;
+        }
+        if (Input.GetAxis("10") != 0)
+        {
+            // rotY = Mathf.Clamp(rotY, -60f, 60f);
+            //transform.Rotate(0,rotY * speed *  Time.deltaTime,0);
+            // transform.localEulerAngles = new Vector3(0, rotY *speed * Time.deltaTime, 0);
+            transform.Rotate(0, rotY2, 0);
+            //transform.rotation = rot;
+        }
+
+    }
+}
