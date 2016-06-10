6cf109bdc06be83233d63065e3831af80d605826
Ryan <up702664@myport.ac.uk>
2016-06-10 16:41:11 +0100

    hgu

--- PowerUps.cs
+++ PowerUps.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class PowerUps : MonoBehaviour {
 
+    float RotSpeed = 100.0f;
+    public  GameObject Tank;
+
+    Tank_Movement tank_movement;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        tank_movement = Tank.GetComponent<Tank_Movement>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, RotSpeed * Time.deltaTime);
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == ("Tank(Clone)"))
+        {
+            Destroy(gameObject);
+            tank_movement.maxSpeed = 10.0f;
+            Debug.Log("Tank Collision");
+        }
+
+    }
 }
+
+
+
